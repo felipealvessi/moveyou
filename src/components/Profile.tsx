9ef5854b52fa205fab5cb2fe428ffe1d1b43c86d
Felipe Alves <felipealvesinfor@gmail.com>
2021-02-24 21:49:47 -0300

    refactoring

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/felipealvessi.png" alt="Felipe Alves" />
+      <div>
+        <strong>Felipe Alves</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,23 @@
-import Head from 'next/head'
 import { ExperienceBar } from './../components/ExperienceBar';
+import { Profile } from './../components/Profile';
+
+import styles from '../styles/Pages/Home.module.css';
 
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar />
-    </div>
+
+
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div>
+
+        </div>
+      </section>
+    </div >
   )
 }
